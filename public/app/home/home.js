37d8de223dda0fcd564b12c62bd43959e3f801c2
Dimitar Manovski <toome123@hotmail.co.uk>
2015-11-06 12:54:57 +0200

    add some updates

--- public/app/home/home.js
+++ public/app/home/home.js
@@ -9,13 +9,32 @@ angular.module('myApp.home', ['ngRoute'])
   });
 }])
 
-.controller('HomeCtrl', function($scope,Devices) {
+.controller('HomeCtrl', function($scope,Devices, deviceValue) {
 	$scope.loading = true;
 	Devices.get()
 			.success(function(data) {
 				$scope.deviceData = data;
 				$scope.loading = false;
 			});
+
+
+
+
+	$scope.generateChart = function(deviceName){
+		$scope.data = [];
+		$scope.testData = [];
+		deviceValue.get(deviceName).success(function(data){
+			for (var i = 0; i < 7; i++) {
+				$scope.testData.push(data[i]['value']);
+			};
+			$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  			$scope.series = [deviceName];
+  			$scope.data = [$scope.testData];
+		});
+	};
+
+
+	
 
 
 });
